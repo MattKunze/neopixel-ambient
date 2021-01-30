352ab45d075b7af265efeabbefa2471cfc4615c7
Matt Kunze <matt.kunze@gmail.com>
2021-01-30 19:56:50 +0000

    add mqtt listener support

--- packages/sprite-server/app.py
+++ packages/sprite-server/app.py
@@ -3,6 +3,7 @@ import neopixel
 from flask import Flask, abort, jsonify, request
 from flask_cors import CORS
 from matplotlib import colors
+import paho.mqtt.client as mqtt
 
 NUM_PIXELS = 64
 pixels = neopixel.NeoPixel(
@@ -13,6 +14,9 @@ pixels = neopixel.NeoPixel(
 
 app = Flask(__name__)
 CORS(app)
+
+MQTT_HOST = 'mqtt'
+MQTT_TOPIC = 'sprite'
 
 @app.route('/parse/<string:str>')
 def parse(str):
@@ -42,6 +46,34 @@ def sprite():
   pixels.show()
 
   return "ok"
+
+def on_connect(client, userdate, flags, rc):
+  print("Connected to MQTT: " + str(rc))
+
+  mqtt_client.subscribe(MQTT_TOPIC + "/#")
+
+def on_message(client, userdata, msg):
+  payload = msg.payload.decode('UTF-8')
+  topic = msg.topic.split('/')
+  print(msg.topic + " " + payload)
+  try:
+    if(topic[1] == 'off'):
+      off()
+    elif(topic[1] == 'fill'):
+      fill(payload)
+    elif(topic[1] == 'sprite'):
+      colors = payload.split(',')
+      for x in range(0, min(len(colors), NUM_PIXELS)):
+        pixels[x] = parse_pixel(colors[x])
+      pixels.show()
+  except ValueError:
+    print("Invalid payload")
+
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.connect(MQTT_HOST, 1883, 60)
+mqtt_client.loop_start()
 
 def brightness(num):
   return int(num * 255)
---
+++ packages/sprite-server/requirements.py
@@ -0,0 +1,7 @@
+spidev
+rpi_ws281x
+adafruit-circuitpython-neopixel
+flask
+flask-cors
+matplotlib
+paho-mqtt
